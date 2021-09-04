b7887e7ef10cbc876b31f2ef3b6f6207bd7882f0
marc <mreverson91@gmail.com>
2021-09-04 13:33:54 -0700

    frontend push

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">About</h5>
+                <p className="cursor-pointer">How Airbnb works</p>
+                <p className="cursor-pointer">Newsroom</p>
+                <p className="cursor-pointer">Investors</p>
+                <p className="cursor-pointer">Airbnb Plus</p>
+                <p className="cursor-pointer">Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p className="cursor-pointer">Accessibility</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+                <p className="cursor-pointer">Lorem Ipsum</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Support</h5>
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
